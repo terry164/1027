12ce03d91f14455507ca00fa193ed8032e0fbaf1
terry164 <tery921616@gmail.com>
2025-11-01 09:56:16 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,206 @@
+let objs = [];
+let colors = ['#f71735', '#f7d002', '#1A53C0', '#232323'];
+let sideMenu; // 用於存放選單的 DOM 元素
+const menuWidth = 350; // 選單寬度，應與 CSS 中的 width 一致
+let iframeContainer;
+let contentIframe;
+let iframeCloseBtn;
+let linkWork;
+let linkNotes;
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	rectMode(CENTER);
+	objs.push(new DynamicShape());
+	// 透過 p5.js 的 select() 函數選取 HTML 中的選單元素
+	sideMenu = select('#side-menu');
+
+	// 選取 Iframe 相關元素
+	iframeContainer = select('#iframe-container');
+	contentIframe = select('#content-iframe');
+	iframeCloseBtn = select('#iframe-close-btn');
+	linkWork = select('#link-work');
+	linkNotes = select('#link-notes');
+
+	// 綁定點擊事件
+	linkWork.mousePressed(function() {
+		showIframe('https://terry164.github.io/9/');
+	});
+
+	linkNotes.mousePressed(function() {
+		showIframe('https://hackmd.io/DPf1tY3-SMiKX5CnYtUsPQ');
+	});
+
+	iframeCloseBtn.mousePressed(hideIframe);
+}
+
+function draw() {
+	background('skyblue');
+}
+
+function draw() {
+	background('skyblue');
+	for (let i of objs) {
+		i.run();
+	}
+
+	if (frameCount % int(random([15, 30])) == 0) {
+		let addNum = int(random(1, 30));
+		for (let i = 0; i < addNum; i++) {
+			objs.push(new DynamicShape());
+		}
+	}
+
+	// 當 iframe 隱藏時才偵測選單
+	if (iframeContainer.hasClass('hidden')) {
+		// 偵測滑鼠位置以顯示或隱藏選單
+		if (mouseX < 100) {
+			// 當滑鼠進入左側 100px 範圍內，為選單加上 'visible' class
+			sideMenu.addClass('visible');
+		} else if (mouseX > menuWidth) {
+			// 當滑鼠離開選單寬度範圍，移除 'visible' class
+			sideMenu.removeClass('visible');
+		}
+	} else {
+		// 當 iframe 顯示時，隱藏選單
+		sideMenu.removeClass('visible');
+	}
+
+	// Iterate backwards to safely remove items from the array
+	for (let i = objs.length - 1; i >= 0; i--) {
+		if (objs[i].isDead) {
+			objs.splice(i, 1);
+		}
+	}
+}
+
+function showIframe(url) {
+	contentIframe.attribute('src', url);
+	iframeContainer.removeClass('hidden');
+	noLoop(); // 暫停 p5.js 動畫
+}
+
+function hideIframe() {
+	contentIframe.attribute('src', '');
+	iframeContainer.addClass('hidden');
+	loop(); // 恢復 p5.js 動畫
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+function easeInOutExpo(x) {
+	return x === 0 ? 0 :
+		x === 1 ?
+		1 :
+		x < 0.5 ? Math.pow(2, 20 * x - 10) / 2 :
+		(2 - Math.pow(2, -20 * x + 10)) / 2;
+}
+
+class DynamicShape {
+	constructor() {
+		this.x = random(0.3, 0.7) * width;
+		this.y = random(0.3, 0.7) * height;
+		this.reductionRatio = 1;
+
+
+		this.shapeType = int(random(5));
+		this.animationType = 0;
+		this.maxActionPoints = int(random(2, 5));
+		this.actionPoints = this.maxActionPoints;
+		this.elapsedT = 0;
+		this.size = 0;
+		this.sizeMax = width * random(0.01, 0.05);
+		this.fromSize = 0;
+		this.init();
+		this.isDead = false;
+		this.clr = random(colors);
+		this.changeShape = true;
+		this.ang = int(random(2)) * PI * 0.25;
+		this.lineSW = 0;
+	}
+
+	show() {
+		push();
+		translate(this.x, this.y);
+		if (this.animationType == 1) scale(1, this.reductionRatio);
+		if (this.animationType == 2) scale(this.reductionRatio, 1);
+		fill(this.clr);
+		stroke(this.clr);
+		strokeWeight(this.size * 0.05);
+		if (this.shapeType == 0) {
+			noStroke();
+			circle(0, 0, this.size);
+		} else if (this.shapeType == 1) {
+			noFill();
+			circle(0, 0, this.size);
+		} else if (this.shapeType == 2) {
+			noStroke();
+			rect(0, 0, this.size, this.size);
+		} else if (this.shapeType == 3) {
+			noFill();
+			rect(0, 0, this.size * 0.9, this.size * 0.9);
+		} else if (this.shapeType == 4) {
+			line(0, -this.size * 0.45, 0, this.size * 0.45);
+			line(-this.size * 0.45, 0, this.size * 0.45, 0);
+		}
+		pop();
+		strokeWeight(this.lineSW);
+		stroke(this.clr);
+		line(this.x, this.y, this.fromX, this.fromY);
+	}
+
+	move() {
+		let n = easeInOutExpo(norm(this.elapsedT, 0, this.duration));
+		if (0 < this.elapsedT && this.elapsedT < this.duration) {
+			if (this.actionPoints == this.maxActionPoints) {
+				this.size = lerp(0, this.sizeMax, n);
+			} else if (this.actionPoints > 0) {
+				if (this.animationType == 0) {
+					this.size = lerp(this.fromSize, this.toSize, n);
+				} else if (this.animationType == 1) {
+					this.x = lerp(this.fromX, this.toX, n);
+					this.lineSW = lerp(0, this.size / 5, sin(n * PI));
+				} else if (this.animationType == 2) {
+					this.y = lerp(this.fromY, this.toY, n);
+					this.lineSW = lerp(0, this.size / 5, sin(n * PI));
+				} else if (this.animationType == 3) {
+					if (this.changeShape == true) {
+						this.shapeType = int(random(5));
+						this.changeShape = false;
+					}
+				}
+				this.reductionRatio = lerp(1, 0.3, sin(n * PI));
+			} else {
+				this.size = lerp(this.fromSize, 0, n);
+			}
+		}
+
+		this.elapsedT++;
+		if (this.elapsedT > this.duration) {
+			this.actionPoints--;
+			this.init();
+		}
+		if (this.actionPoints < 0) {
+			this.isDead = true;
+		}
+	}
+
+	run() {
+		this.show();
+		this.move();
+	}
+
+	init() {
+		this.elapsedT = 0;
+		this.fromSize = this.size;
+		this.toSize = this.sizeMax * random(0.5, 1.5);
+		this.fromX = this.x;
+		this.toX = this.fromX + (width / 10) * random([-1, 1]) * int(random(1, 4));
+		this.fromY = this.y;
+		this.toY = this.fromY + (height / 10) * random([-1, 1]) * int(random(1, 4));
+		this.animationType = int(random(3));
+		this.duration = random(20, 50);
+	}
+}
